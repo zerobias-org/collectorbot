9a8609f488010855882a907ed81bb77eb4c93d34
Catalin Tamas <ctamas@zerobias.com>
2025-05-29 13:33:05 +0300

    feat(tls): add tls collectorbot

---
+++ package/auditmation/generic/tls/src/index.ts
@@ -0,0 +1,5 @@
+import { CollectorAuditmationGenericTlsImpl } from './CollectorAuditmationGenericTlsImpl';
+
+export default CollectorAuditmationGenericTlsImpl;
+/* eslint-disable */
+export * from '../generated';
