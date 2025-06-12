503734b16afa1b9f5505ae0f96b97afb426d8387
Catalin Tamas <ctamas@zerobias.com>
2025-06-12 14:19:05 +0300

    fix(tls): use types on mapping

--- package/auditmation/generic/tls/src/Mappers.ts
+++ package/auditmation/generic/tls/src/Mappers.ts
@@ -1,5 +1,11 @@
 import { ServerConfiguration, X509Certificate } from '@auditlogic/module-auditmation-generic-tls';
-import { ServiceEndpoint, X509Certificate as X509CertificateSchema } from '@auditlogic/schema-auditmation-auditmation-base-ts';
+import {
+  ServiceEndpoint,
+  ServiceEndpoint_cipherSuites,
+  ServiceEndpoint_sslVersions,
+  ServiceEndpoint_tlsVersions,
+  X509Certificate as X509CertificateSchema
+} from '@auditlogic/schema-auditmation-auditmation-base-ts';
 import { InvalidInputError, URL } from '@auditmation/types-core-js';
 
 export function toX509Certificate(cert: X509Certificate): X509CertificateSchema {
@@ -31,27 +37,27 @@ export function toX509Certificate(cert: X509Certificate): X509CertificateSchema 
   return output;
 }
 
-function toSslEnum(sslVersion: string): string {
+function toSslEnum(sslVersion: string): ServiceEndpoint_sslVersions {
   switch (sslVersion) {
     case 'sslv2':
-      return 'SSL_2';
+      return ServiceEndpoint_sslVersions.SSL_2;
     case 'sslv3':
-      return 'SSL_3';
+      return ServiceEndpoint_sslVersions.SSL_3;
     default:
       throw new InvalidInputError('SSL version', sslVersion);
   }
 }
 
-function toTlsEnum(tlsVersion: string): string {
+function toTlsEnum(tlsVersion: string): ServiceEndpoint_tlsVersions {
   switch (tlsVersion) {
     case 'tls1_0':
-      return 'TLS_1_0';
+      return ServiceEndpoint_tlsVersions.TLS_1_0;
     case 'tls1_1':
-      return 'TLS_1_1';
+      return ServiceEndpoint_tlsVersions.TLS_1_1;
     case 'tls1_2':
-      return 'TLS_1_2';
+      return ServiceEndpoint_tlsVersions.TLS_1_2;
     case 'tls1_3':
-      return 'TLS_1_3';
+      return ServiceEndpoint_tlsVersions.TLS_1_3;
     default:
       throw new InvalidInputError('TLS version', tlsVersion);
   }
@@ -67,22 +73,16 @@ export function toServiceEndpoint(url: URL, config: ServerConfiguration, certSn?
     .filter((p) => p.startsWith('ssl'))
     .map(toSslEnum);
   const cipherSuites = config.cipherSuites
-    ?.map((cs) => cs.description?.toString().toUpperCase());
+    ?.map((cs) => ServiceEndpoint_cipherSuites[`${cs.description?.toString().toUpperCase()}`]);
 
   const output: ServiceEndpoint = {
     id: `${url}`,
     name: `${url}`,
+    tlsCertificates: certSn,
+    tlsVersions,
+    sslVersions,
+    cipherSuites,
   };
-
-  Object.assign(
-    output,
-    {
-      tlsCertificate: certSn,
-      tlsVersions,
-      sslVersions,
-      cipherSuites,
-    }
-  );
 
   return output;
 }
