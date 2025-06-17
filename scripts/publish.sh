a077443e4e8b2a36b9a822eedc73f119fe184748
Catalin Tamas <ctamas@zerobias.com>
2025-06-17 15:59:24 +0300

    chore(release): bump version

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,7 +6,7 @@ NPM_TOKEN=$READ_TOKEN npm i
 NPM_TOKEN=$READ_TOKEN npm shrinkwrap
 
 NAME=$(jq -r '.name' package.json)
-VERSION=$(jq -r '.version' package.json)
+VON=$(jq -r '.version' package.json)
 
 LATEST=$(npm view $NAME dist-tags.latest) || echo ""
 
