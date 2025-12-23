f9dd8b90f9faab378601b78015e53703a76225a1
Catalin Tamas <ctamas@zerobias.com>
2025-12-23 15:58:33 +0200

    feat: add dynamic collector base

---
+++ package/zerobias/zerobias/dynamic/src/Mappers.ts
@@ -0,0 +1,21 @@
+/**
+ * Mapper for multiple element operations
+ * Returns PagedResults<Object> from getChildren, objectSearch, getCollectionElements, searchCollectionElements
+ *
+ * TODO: Replace with actual mapping logic when schema is defined
+ */
+export function toMultipleElements(obj: any): any {
+  // Placeholder mapping - returns object as-is
+  return obj;
+}
+
+/**
+ * Mapper for single element operations
+ * Returns Object from getRootObject, getObject, getCollectionElement
+ *
+ * TODO: Replace with actual mapping logic when schema is defined
+ */
+export function toSingleElement(obj: any): any {
+  // Placeholder mapping - returns object as-is
+  return obj;
+}
---
+++ package/zerobias/zerobias/dynamic/src/index.ts
@@ -0,0 +1,5 @@
+import { CollectorZerobiasZerobiasDynamicImpl } from './CollectorZerobiasZerobiasDynamicImpl';
+
+export default CollectorZerobiasZerobiasDynamicImpl;
+// eslint-disable-next-line
+export * from '../generated';
