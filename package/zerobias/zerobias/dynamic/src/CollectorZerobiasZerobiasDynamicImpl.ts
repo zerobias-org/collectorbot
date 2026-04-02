f9ab46263612f49f58c1eded42c0c98bc231fd11
Catalin Tamas <ctamas@zerobias.com>
2026-04-02 15:54:13 +0300

    fix(dynamic): log mapped item instead of raw one in case of error

--- package/zerobias/zerobias/dynamic/src/CollectorZerobiasZerobiasDynamicImpl.ts
+++ package/zerobias/zerobias/dynamic/src/CollectorZerobiasZerobiasDynamicImpl.ts
@@ -125,7 +125,7 @@ export class CollectorZerobiasZerobiasDynamicImpl extends BaseClient {
           errorCount++;
           await batch.error(
             `Failed to process item: ${(err as Error).message}`,
-            item
+            mappedItem
           );
         }
       }
